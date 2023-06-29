1c2e1fefa24d8eb1d9dd2ffd1dbbbccc3304244c
Sviatoslav Lyskov <justerhofman25@yandex.ru>
2023-06-29 13:54:31 +0300

    sprint13,iter2

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
